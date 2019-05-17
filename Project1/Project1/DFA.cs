60f9fb19ae60d02a49bc3712f918290b3efa488f
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2019-05-17 08:15:59 -0700

    Remove unreachables

---
+++ Project1/Project1/DFA.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Project1
+{
+    public class DFA
+    {
+        int StateCount { get { return this.AdjacencyList.Length; } }
+        string[] Alphabet;
+        HashSet<int>[] AdjacencyList;
+        char[,] Cost;
+        int StartState;
+        HashSet<int> FinalStates;
+
+        public DFA(
+            string[] alphabet,
+            HashSet<int>[] adjacencyList,
+            char[,] cost,
+            int startState,
+            HashSet<int> finalStates
+            )
+        {
+            this.Alphabet = alphabet;
+            this.AdjacencyList = adjacencyList;
+            this.Cost = cost;
+            this.StartState = startState;
+            this.FinalStates = finalStates;
+        }
+
+        public DFA Minimize()
+        {
+            // first determine unreachable states
+            DFA minimizedDFA = new DFA(
+                this.Alphabet,
+                this.AdjacencyList,
+                this.Cost,
+                this.StartState,
+                this.FinalStates
+                );
+            bool[] reachable = minimizedDFA.DetermineReachables();
+
+            // remove reachAbles
+            for (int i = 0; i < reachable.Length; i++)
+            {
+                if (reachable[i] == false)
+                {
+                    minimizedDFA.AdjacencyList[i] = null;
+                }
+            }
+
+            bool[,] equality = new bool[StateCount, StateCount];
+
+
+            for (int i = 1; i < StateCount && reachable[i] == true; i++)
+            {
+                for (int j = 0; j < StateCount - 1 && reachable[j] == true; j++)
+                {
+                    if (minimizedDFA.checkEquality(i, j) == 0)
+                    {
+                    }
+                }
+            }
+
+
+            return minimizedDFA;
+        }
+
+        public int checkEquality(int node1, int node2)
+        {
+            for (int i = 0; i < this.Alphabet.Length; i++)
+            {
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Determine reachability for states.
+        /// ture --> reachable
+        /// false --> unreachable
+        /// </summary>
+        /// <returns></returns>
+        public bool[] DetermineReachables()
+        {
+            bool[] reachable = new bool[this.StateCount];
+            Queue<int> q = new Queue<int>();
+
+            q.Enqueue(StartState);
+            reachable[StartState] = true;
+
+            // BFS
+            while (q.Count != 0)
+            {
+                int node = q.Dequeue();
+                for (int i = 0; i < this.AdjacencyList[node].Count; i++)
+                {
+                    if (reachable[this.AdjacencyList[node].ElementAt(i)] == false)
+                    {
+                        q.Enqueue(this.AdjacencyList[node].ElementAt(i));
+                        reachable[this.AdjacencyList[node].ElementAt(i)] = true;
+                    }
+                }
+            }
+            return reachable;
+        }
+    }
+}
--- Project1/Project1/NFA.cs
+++ Project1/Project1/NFA.cs
@@ -3,39 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using System.IO;
 
 namespace Project1
 {
     public class NFA
     {
-        string[] alphabet;
-        HashSet<int>[] adjacencyList;
-        List<char>[,] cost;
-        int startState;
-        HashSet<int> finalStates;
+        string[] Alphabet;
+        HashSet<int>[] AdjacencyList;
+        List<char>[,] Cost;
+        int StartState;
+        HashSet<int> FinalStates;
 
         public NFA(string path)
         {
             string[] fileLines = File.ReadAllLines(path);
             int stateCount = int.Parse(fileLines[0]);
-            // earn alphabet 
-            this.alphabet = fileLines[1].Split(' ', ',');
+            // earn Alphabet 
+            this.Alphabet = fileLines[1].Split(' ', ',');
 
-            // initialize adjacencyList 
-            this.adjacencyList = new HashSet<int>[stateCount];
-            for (int i = 0; i < adjacencyList.Length; i++)
-                this.adjacencyList[i] = new HashSet<int>();
+            // initialize AdjacencyList 
+            this.AdjacencyList = new HashSet<int>[stateCount];
+            for (int i = 0; i < AdjacencyList.Length; i++)
+                this.AdjacencyList[i] = new HashSet<int>();
 
-            // initialize cost
-            this.cost = new List<char>[stateCount, stateCount];
+            // initialize Cost
+            this.Cost = new List<char>[stateCount, stateCount];
             for (int i = 0; i < stateCount; i++)
                 for (int j = 0; j < stateCount; j++)
-                    this.cost[i,j] = new List<char>();
-            // determine startState and finalStates
-            this.startState = 0;
-            this.finalStates = new HashSet<int>();
+                    this.Cost[i,j] = new List<char>();
+            // determine StartState and FinalStates
+            this.StartState = 0;
+            this.FinalStates = new HashSet<int>();
             // create adjacency list
             for (int i = 2; i < fileLines.Length; i++)
             {
@@ -44,15 +43,15 @@ namespace Project1
                 int t = int.Parse(edge[2][edge[2].Length - 1].ToString());
 
                 if (edge[0][0] == '-')
-                    this.startState = s;
+                    this.StartState = s;
                 if (edge[0][0] == '*')
-                    this.finalStates.Add(s);
+                    this.FinalStates.Add(s);
                 if (edge[2][0] == '-')
-                    this.startState = t;
+                    this.StartState = t;
                 if (edge[2][0] == '*')
-                    this.finalStates.Add(t);
-                this.adjacencyList[s].Add(t);
-                this.cost[s, t].Add(edge[1][0]);
+                    this.FinalStates.Add(t);
+                this.AdjacencyList[s].Add(t);
+                this.Cost[s, t].Add(edge[1][0]);
             }
         }
     }
